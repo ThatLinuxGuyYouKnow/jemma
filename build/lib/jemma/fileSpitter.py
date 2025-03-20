603cf8330dbacdf14b71c98557e45fccaaf247d9
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-03-20 10:54:45 +0100

    feature: renamed start commands

---
+++ build/lib/jemma/fileSpitter.py
@@ -0,0 +1,12 @@
+import os
+
+def spitAllFiles(files):
+  for file in files:
+   if os.path.isdir(file):
+    filess = os.listdir(file)
+    print ('parent directory = ' + file + '***constituent files = ' + str(os.listdir(file)))
+    for secondLayer in filess:
+     spitAllFiles(secondLayer)
+   elif os.path.isfile:
+    print(file)
+ 
