8856457c14ae9f6c750f30c5ca7c051a50a4d985
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-03-22 14:22:38 +0100

    feature: logic for continuing chat

--- jemma/__pycache__/startSession.py
+++ jemma/__pycache__/startSession.py
@@ -1,7 +1,12 @@
 def startCodeSession(firstPrompt : str):
     try:
-     with open('./current_chat', "w") as f:
+     with open('.current_chat.txt', "w") as f:
         f.write(firstPrompt)
     except PermissionError:
        print('Could not strat a chat session')
-    
+
+
+def continueChat(newPrompt:str, chatHistory):
+    try:
+     with open('current_chat.txt', 'r') as f:
+        f.read
