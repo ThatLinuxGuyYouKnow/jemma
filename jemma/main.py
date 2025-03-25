bb88b04f5c60b83599e6f89945602f0c47099e61
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-03-25 22:00:54 +0100

    chore: renamed chat arg

--- jemma/main.py
+++ jemma/main.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 
 from jemma.initialization import initialize_jemma
+from jemma.utils.getApiKey import get_api_key
 from jemma.utils.getFilesContent import get_files_content
 from .utils.fileSpitter import spitAllFiles
 from .model.explainCodebase import explainCode
@@ -11,12 +12,12 @@ from .model.startSession import startCodeSession
 def main():
      parser = argparse.ArgumentParser(description="Get coding help right in your terminal!")
      parser.add_argument("-e", "--explain", action="store_true", help="Explain this repository, provide an overview of critical functions and/or views")
-     parser.add_argument("-s", "--start", action="store_true", help="Start an interactive session, no access to codebase")
+     parser.add_argument("-ch", "--chat", action="store_true", help="Start an interactive session, no access to codebase")
      parser.add_argument("-init", "--initialize", action="store_true", help="Explain this repository, provide an overview of critical functions and/or views")
      parser.add_argument("output", nargs="?", default="README.md", help="Output file path (default: README.md)")
      content = get_files_content()
      args = parser.parse_args()
-     if args.start:
+     if args.chat:
          print('Alright lets get started!')
          firstPrompt = input('>')
          startCodeSession(firstPrompt)
