f1cbaca90e2f04022e3bcbd9e0021ecf574fe464
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-03-25 23:04:11 +0100

    feature: better error handling in model interactions

--- jemma/model/modelInteraction.py
+++ jemma/model/modelInteraction.py
@@ -2,8 +2,10 @@ import json
 import requests
 
 from jemma.utils.getApiKey import get_api_key
-from jemma.utils.terminalPrettifier import responseFormatter
+from jemma.utils.terminalPrettifier import errorText, warningText
+from requests.exceptions import RequestException 
 def modelInteraction(prompt: str):
+ try:
     apikey = get_api_key()
     payload = {
         "contents": [
@@ -22,16 +24,18 @@ def modelInteraction(prompt: str):
     response_data = response.json()
     print(response_data)
     (response)
-    if response.status_code != 200: ## remeber to do stuff if it returns 400 for a bad api key
+    if response.status_code == 400:
+       print(errorText("Error occured, Your api key likely isn't valid, run")+warningText(' jemma-configure ')+ errorText('to re-enter your key'))
+    if response.status_code != 200:
+        print(errorText('An error occured, please try again in a bit'))
         print (str(response.status_code))
     if 'candidates' in response_data and len(response_data['candidates']) > 0:
         response=  response_data['candidates'][0]['content']['parts'][0]['text']
-        
-        if response == None:
-            print('response is empty')
-        print(responseFormatter(response))
         return response
     else:
-        print('somrthing went wrong')
-        print('Please start a new session')
+        print(errorText('something went wrong and the model did not return a response'))    
         quit()    
+ except RequestException:
+    print(errorText("You need a working Internet Connection to use jemma"))
+ except Exception as e:
+    print(errorText('An unexpected error occured, please try again'))
