c84da7ea772e34f416023e600700ef2666ea34a8
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-03-30 21:08:19 +0100

    feature: improved watch command

--- jemma/utils/commandWatch.py
+++ jemma/utils/commandWatch.py
@@ -1,5 +1,21 @@
 import os
+import subprocess
+
+from jemma.model.modelInteraction import modelInteraction
 
 
-def watchCommand(functionToRun):
-   os.system(functionToRun)
+def watchCommand(functionToRun, directoryStructure: str, codeContent: str):
+   result = subprocess.run(
+                functionToRun.split(),
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True
+            )
+   command_result = result.stdout
+   model_prompt=f""" You are to return a response based on the codebase and resulting terminal output 
+   code content: ${codeContent}
+   Directory Structure: ${directoryStructure}
+   Terminal Output: ${command_result}"""
+   modelInteraction(prompt="")
+            print(f"\nCommand output:\n{result.stdout}")
