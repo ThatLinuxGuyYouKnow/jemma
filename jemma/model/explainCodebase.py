5628d529558a48622e4309c02b0a990b3861b094
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-04-03 00:08:05 +0100

    chore: tidied --explain code

--- jemma/model/explainCodebase.py
+++ jemma/model/explainCodebase.py
@@ -15,21 +15,9 @@ def explainCode(directoryStructure: str, apikey: str, files: str):
     - File Content :=  ${files}"""
  
     response= modelInteraction(model_prompt)
- 
- 
-    if response.status_code == 400:
-        print(errorText("Your api key likely isn't valid, run " +warningText("gemma-configure ")+ errorText("to enter a new one")))
-        return None
-    if response.status_code != 200:
-        print(errorText("Something went wrong with your request, try again"))
-        return None
-    if 'candidates' in response_data and len(response_data['candidates']) > 0:
-        response=  response_data['candidates'][0]['content']['parts'][0]['text']
-        print(responseFormatter(response))
-    else:
-       print(errorText("An error occured and the model did not return a response"))
- except RequestException:
-    print(errorText('You need a working Internet connection to use jemma'))
+    if response == None:
+       print(errorText('Something went wrong'))
+    print(responseFormatter(response))
  except Exception as e:
-    print(errorText('an error occured, please try again'))
+  print(errorText('An error occured '+ str(e)))
  
