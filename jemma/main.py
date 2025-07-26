edb836ef2cb1c87eb85ce4ed5639010996a2089e
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-07-26 16:37:37 +0100

    refactor: all args will now run only one at a time to prevent ravce conditions

--- jemma/main.py
+++ jemma/main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import signal
 import sys
  
+from jemma.config import configure_jemma
 from jemma.model.commandWatch import watchCommand
 from jemma.model.editCode import editCode
 from jemma.utils.getApiKey import get_api_key
@@ -52,32 +53,33 @@ if not hasattr(sys, 'cleanup_registered'):
 def main():
     try:
         parser = argparse.ArgumentParser(description="Get coding help right in your terminal!")
+         
         parser.add_argument("-ex", "--explain", action="store_true", help="Explain this repository, provide an overview of critical functions and/or views")
         parser.add_argument("-ch", "--chat", action="store_true", help="Start an interactive session, no access to codebase")
         parser.add_argument("-config", "--configure", action="store_true", help="Start your jemma experience, set preferences")
         parser.add_argument("-w", "--watch", nargs='+', help="Jemma will run this command watch the output for you")
         parser.add_argument("-ed", "--edit", type=str, nargs='+', help="Let Jemma help you fix bugs and add features")
         parser.add_argument("output", nargs="?", default="README.md", help="Output file path (default: README.md)")
-        
+
         args = parser.parse_args()
-        
+
         # Get API key and check if it exists
         apiKey: str = get_api_key()
         if not apiKey:
             print("You'll need to setup your api key first to use Jemma, Please run "+ successText('jemma-init'))
             return 1
-        
-        # Get directory structure and file contents
+
+ 
         if args.chat:
-            print('Hallo!, lets get started!')
-            firstPrompt = input('>')
-            chat_loop(firstPrompt)
+            chat_loop()
+            return 0  
+     
         path = os.getcwd()
         dc = os.listdir(path)
         ds = spitAllFiles(dc)
         content = get_files_content()
-        
-        # Process arguments
+
+     
         if args.watch:
             commandToRun = "".join(args.watch)
             watchCommand(functionToRun=commandToRun, directoryStructure=ds, codeContent=content)
@@ -88,13 +90,13 @@ def main():
             explainCode(directoryStructure=ds, files=content)
         elif args.edit:
             user_prompt = ''.join(args.edit)
-           
             editCode(directoryStructure=ds, fileContents=content, userPrompt=user_prompt)
         else:
+  
             parser.print_help()
-            
+
         return 0
-        
+
     except Exception as e:
         print(errorText(f"An unexpected error occurred: {e}"))
         return 1
