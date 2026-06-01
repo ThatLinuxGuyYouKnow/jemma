e516865a08d37ececcf84b6f9baa5b77c5939da6
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2026-06-01 19:55:29 +0100

    feat: update inference layer to stream deltas with tool schemas

--- jemma/main/inference/main.py
+++ jemma/main/inference/main.py
@@ -1,15 +1,30 @@
 from litellm import completion
+from jemma.tools.registry import TOOL_SCHEMAS
+from jemma.classes.agentic_response import AgentResponse
+from jemma.enums.response_enums import ResponseType
 import os
 
 def get_model_response(model_id: str, messages : list[str]) :
 
-    for chunk in completion(
+    stream = completion(
         model= model_id,
         messages=messages,
-        stream=True
-    ):
+        stream=True,
+        tools = TOOL_SCHEMAS
+    )
+
+    for chunk in stream:
+
+        delta = chunk.choices[0].delta
+
+        yield delta
+
         
-        yield chunk.choices[0].delta.content or ""
+
+
+
+
+            
 
 
 
