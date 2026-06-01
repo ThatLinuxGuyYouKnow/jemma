3374f971102f0b8a028464ae40e17630b96e8e8b
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2026-06-01 19:55:41 +0100

    feat: add streaming tool call accumulation and typed response yielding to agent loop

--- jemma/agent/main.py
+++ jemma/agent/main.py
@@ -1,4 +1,6 @@
 from jemma.main.inference.main import get_model_response 
+from jemma.classes.agentic_response import AgentResponse
+from jemma.enums.response_enums import ResponseType
 
 
 class AgentLoop():
@@ -7,13 +9,65 @@ class AgentLoop():
         self.messages = []    
 
  
-    def start_loop(self, new_message: str):
+    def start_loop(self, new_message: str, model_id : str):
 
-        self.messages.append(new_message)
+        ## add the newest message from the user to the message stack
 
-        response = get_model_response(messages= self.messages)
+        self.messages.append({'role':'user', 'content': new_message})
 
-        if response.tool_calls:
+        accumulated_tool_calls = {}
+
+        response = get_model_response(messages= self.messages, model_id=model_id)
+
+        while True:
+
+            for delta in response:
+
+                if delta.content:
+
+                    yield AgentResponse(
+                        type=ResponseType.TEXT,
+                        content=delta.content
+                    )
+
+                if delta.tool_calls:
+
+                     for tool_call in delta.tool_calls:
+
+                        idx = tool_call.index
+
+                        if idx in accumulated_tool_calls:
+
+                            ## in this case, we've already seen some of this tool call in a previous chunk, we just want to update the funcs args
+
+                            accumulated_tool_calls[idx]["arguments"] += tool_call.function.arguments
+
+                        else:
+
+                            ## we havent seen this function before, we need to start to accumulate it
+
+                            accumulated_tool_calls[idx] = {
+
+                        "id":tool_call.id,
+                        "name":tool_call.function.name or '',
+                        "arguments": tool_call.function.arguments
+                    }
+                            
+                if accumulated_tool_calls:
+
+                    for tool_call in accumulated_tool_calls:
+
+
+                        yield AgentResponse(
+                            type= ResponseType.TOOL_CALL,
+                            content= tool_call['name'] 
+                    )
+
+
+
+
+           
+
 
 
 
