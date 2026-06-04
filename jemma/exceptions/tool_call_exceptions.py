0ea395c2a4673a90f229183c746c8cf71d1add88
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2026-06-04 19:55:37 +0100

    fix: improve tool call exception default messages

--- jemma/exceptions/tool_call_exceptions.py
+++ jemma/exceptions/tool_call_exceptions.py
@@ -1,16 +1,16 @@
 
 class ToolCallException(Exception):
 
-    def __init__(self, *args, model_error_feedback: str):
+    def __init__(self, *args, model_error_feedback: str = "Incorrect Tool use"):
 
         self.user_facing_message = "Model produced a malformed tool call"
-        self.model_error_feedback = "Incorrect Tool use"
+        self.model_error_feedback = model_error_feedback
         super().__init__(*args)
 
 
 class InvalidToolArguments(ToolCallException):
 
-    def __init__(self, *args, model_error_feedback):
+    def __init__(self, *args, model_error_feedback: str = "Invalid arguments provided for this tool, try again with valid arguments"):
         super().__init__(*args, model_error_feedback=model_error_feedback)
 
 
